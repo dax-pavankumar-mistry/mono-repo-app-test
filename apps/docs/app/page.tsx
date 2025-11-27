56492e2c1d6b522c3c5bd063c126af2243d3d76c
dax-pavankumar-mistry <pavankumar.mistry@daxwell.com>
2025-11-26 21:17:36 -0600

    updated

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Testing docs <code>apps/docs/app/page.tsx</code>
+            Testing docs -- 1 <code>apps/docs/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
